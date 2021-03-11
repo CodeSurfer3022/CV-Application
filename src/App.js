8d14aec62f78cbf73955bee52881eb2aa9e18c92
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-03-11 16:47:45 +0530

    Add functionality for adding form and cancel

--- src/App.js
+++ src/App.js
@@ -6,47 +6,47 @@ import Skills from "./Components/Skills";
 import Projects from "./Components/Projects";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            firstName: "First Name",
-            lastName: "Last Name",
-            designation: "Designation",
-            city: "City, Country",
-            phone: "Phone",
-            email: "Email",
-            linkedin: "Linkedin",
-            github: "Github"
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "First Name",
+      lastName: "Last Name",
+      designation: "Designation",
+      city: "City, Country",
+      phone: "Phone",
+      email: "Email",
+      linkedin: "Linkedin",
+      github: "Github"
     }
+  }
 
-    handleChange = (e) => {
-        const value = e.target.value;
-        const id = e.target.parentNode.id;
-        console.log(value, id);
-        this.setState({
-            [id]: value
-        })
-    }
+  handleChange = (e) => {
+    const value = e.target.value;
+    const id = e.target.parentNode.id;
+    console.log(value, id);
+    this.setState({
+      [id]: value
+    })
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <General
-                    handleChange={this.handleChange}
-                    details={this.state}
-                />
-                <Skills/>
-                <Employment
-                    handleChange={this.handleChange}
-                />
-                <Education
-                    handleChange={this.handleChange}
-                />
-                <Projects/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        <General
+          handleChange={this.handleChange}
+          details={this.state}
+        />
+        <Skills/>
+        <Employment
+          handleChange={this.handleChange}
+        />
+        <Education
+          handleChange={this.handleChange}
+        />
+        <Projects/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, {Component} from "react";
 
-function Skills() {
+function handleClick(e) {
+  console.log(e.target);
+  const form = document.querySelector('form[name="skills"]');
+  form.classList.remove('none');
+  e.target.classList.add('none');
+}
+
+function cancel(e) {
+  e.preventDefault();
+  const btn = document.querySelector('#addForm');
+  console.log(btn);
+  const form = document.querySelector('form[name="skills"]');
+  form.classList.add('none');
+  btn.classList.remove('none');
+}
+
+function addSkill() {
+
+}
+
+class Skills extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      skills: []
+    }
+  }
+
+  render() {
     return (
-        <div className="skills">
-            <button>Add skill</button>
-        </div>
+      <div className="skills">
+        <form className="none" name="skills">
+          <div>
+            <label form="skills">Skills</label>
+            <input placeholder="Add your top skills!" name="skills"/>
+            <button onClick={cancel}>Cancel</button>
+            <button onClick={addSkill}>+ skill</button>
+          </div>
+        </form>
+        <button id="addForm" onClick={handleClick}>Add skill</button>
+      </div>
     )
+  }
 }
 
 export default Skills;
